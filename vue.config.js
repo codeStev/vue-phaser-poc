7eaf19a37a53d1d486c221de2c02d74185552828
codestev <steven.hohnke@outlook.com>
2021-05-03 16:23:36 +0200

    increased ram usage for dev server

--- vue.config.js
+++ vue.config.js
@@ -1,52 +1,54 @@
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const os = require('os');
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ],
-  chainWebpack: config => {
-    
-     const imageRule = config.module.rule('images')
-     const gameAssetRule = config.module.rule('gameAssets')
-     const jsonRule = config.module.rule('GameAtlas')
+	transpileDependencies: ['vuetify'],
+	chainWebpack: (config) => {
+		const imageRule = config.module.rule('images');
+		const gameAssetRule = config.module.rule('gameAssets');
+		//const jsonRule = config.module.rule('GameAtlas')
 
-    //ggf noch fallback
-    imageRule.uses.clear()
-    gameAssetRule.uses.clear()
-    imageRule
-    .test(/\.(png|jpe?g|gif|webp)(\?.*)?$/)
-    .exclude
-    .add(/node_modules/)
-    .add(/src\/assets\/gameAssets/)
-    .add(/src\\assets\\gameAssets/)
-    .end()
-    .use('url-loader')
-    .loader('url-loader')
-    .tap(options => {
-      limit = 4096
-      // fallback = 'file-loader' 
-      // fallback.options.name = 'img/[name].[hash:8].[ext]'
-      return options
-    })
-    .end()
-    // // add replacement loader(s)
-     gameAssetRule
-     .test(/\.(jpe?g|png|gif|svg)$/i)
-     .include
-     .add(/src\/assets\/gameAssets/)
-     .add(/src\\assets\\gameAssets/)
-     .end()
-       .use('file-loader')
-         .loader('file-loader?name=[name].[ext]')
-         .end()
+		//ggf noch fallback
+		imageRule.uses.clear();
+		gameAssetRule.uses.clear();
+		imageRule
+			.test(/\.(png|jpe?g|gif|webp)(\?.*)?$/)
+			.exclude.add(/node_modules/)
+			.add(/src\/assets\/gameAssets/)
+			.add(/src\\assets\\gameAssets/)
+			.end()
+			.use('url-loader')
+			.loader('url-loader')
+			.tap((options) => {
+				limit = 4096;
+				// fallback = 'file-loader'
+				// fallback.options.name = 'img/[name].[hash:8].[ext]'
+				return options;
+			})
+			.end();
+		// // add replacement loader(s)
+		gameAssetRule
+			.test(/\.(jpe?g|png|gif|svg)$/i)
+			.include.add(/src\/assets\/gameAssets/)
+			.add(/src\\assets\\gameAssets/)
+			.end()
+			.use('file-loader')
+			.loader('file-loader?name=[name].[ext]')
+			.end();
 
-      // jsonRule
-      // .test(/\.(json)$/i)
-      // .include
-      // .add(/src\/assets\/gameAssets/)
-      // .end()
-      //   .use('json-loader')
-      //     .loader('json-loader?name=[name].[ext]')
-      //     .end()
-      
-  }
-  
-}
+		// jsonRule
+		// .test(/\.(json)$/i)
+		// .include
+		// .add(/src\/assets\/gameAssets/)
+		// .end()
+		//   .use('json-loader')
+		//     .loader('json-loader?name=[name].[ext]')
+		//     .end()
+		config.plugin('fork-ts-checker').tap((args) => {
+			let totalmem = Math.floor(os.totalmem() / 1024 / 1024); //get OS mem size
+			let allowUseMem = totalmem > 12000 ? 8192 : 1000;
+			// in vue-cli shuld args[0]['typescript'].memoryLimit
+			args[0].memoryLimit = allowUseMem;
+			return args;
+		});
+	},
+};
