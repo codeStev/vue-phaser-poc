98af6f72e8cd199587b065f9c300b823be35da63
Arthur Hej <82497746+arthurhej@users.noreply.github.com>
2021-05-18 10:33:57 +0200

    axios added; rest-api base component added;

---
+++ src/components/ApiConnect.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const apiClient = {
+  async readAllScores() {
+    const response = await axios.get("/scores");
+    return response.data;
+  },
+  async readTopTenScores() {
+    const response = await axios.get("/topten");
+    return response.data;
+  },
+  async createScore(newScore: any) {
+    const response = await axios.post("/scores", newScore);
+    return response.data;
+  },
+  async deleteScore(scoreId: any) {
+    const response = await axios.delete("/scores" + scoreId);
+    return response.data;
+  },
+};
+
+export default apiClient;
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify';
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
 
-Vue.config.productionTip = false
+//axios -> see https://javatodev.com/vue-js-crud-with-vuetify/
+import axios from "axios";
+import VueAxios from "vue-axios";
+
+axios.defaults.baseURL = "http://localhost:8080/api";
+
+//axios
+Vue.use(VueAxios, axios);
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
